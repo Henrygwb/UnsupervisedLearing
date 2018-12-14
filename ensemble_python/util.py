1435b2e1160d7f8e5ee7a865a33231930e75d506
Henrygwb <henrygwb@gmail.com>
2018-12-14 18:10:01 -0500

    add some codes

--- ensemble_python/util.py
+++ ensemble_python/util.py
@@ -8,8 +8,27 @@ def gendata(n, p, c, k):
     :param k: number of small clusters
     :return:
     """
-    pp = np.sort(np.random.uniform(0,c-1,3))
+    pp = np.sort(np.random.uniform(0,1,c-1))
     pp = np.append(pp,[1]) - np.insert(pp,0,0)
-    mu = np.zeros((p, c))
-    mu
+    mu = np.array(np.random.uniform(-5,5,c*p) (p, c))
+    sigma_true = np.zeros((p,p,c))
+    for i in xrange(c):
+        
 
+
+
+V = list()
+for (ii in 1:C){
+    # Sigma_true[,,ii] = riwish(p+13, diag(5, p))
+    V[[ii]] = genPositiveDefMat(dim=p)$Sigma
+Sigma_true[, , ii] = riwish(5 * p, V[[ii]])
+}
+
+rsample < - rmultinom(n, 1, pp_true)  ## C by n, indicator of sample
+z_true < - apply(rsample, 2, function(x)
+which(x == 1))
+X < - t(sapply(z_true, function(x)
+{rmvnorm(1, mu_true[, x], Sigma_true[,, x])}))  # nxp
+
+return (list(X=X, z=z_true))
+}
