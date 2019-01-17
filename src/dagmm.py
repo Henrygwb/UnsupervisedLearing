3d387b43435e9610b4b0a7541f54c2d480099268
Henrygwb <henrygwb@gmail.com>
2019-01-17 18:07:52 -0500

    add dagmm before debug

--- src/dagmm.py
+++ src/dagmm.py
@@ -6,7 +6,7 @@ from util import metrics
 metrics = metrics()
 from os import makedirs
 from os.path import exists, join
-
+import math
 class GAGMM(object):
     def __init__(self,
                  compress_hidden,
@@ -18,7 +18,8 @@ class GAGMM(object):
                  compress_activation = tf.nn.tanh,
                  estimate_activation = tf.nn.tanh,
                  dropout_rate = 0.5,
-                 use_dropout = 1
+                 use_dropout = 1,
+                 use_diag_cov = 0
                  ):
         self.input_dim = compress_hidden[0]
         self.compress_hidden = compress_hidden
@@ -31,6 +32,7 @@ class GAGMM(object):
         self.estimate_activation = estimate_activation
         self.dropout_rate = dropout_rate
         self.use_dropout = use_dropout
+        self.use_diag_cov = use_diag_cov
 
     def build_compress(self, x):
         n_layers = len(self.compress_hidden) - 1
@@ -73,7 +75,54 @@ class GAGMM(object):
         return gamma
 
     def build_gmm(self, z, gamma):
-        
+        with tf.variable_scope('gmm'):
+            # phi: [num_mixture, ]
+            phi = tf.reduce_mean(gamma, axis=0)
+            phi_sum = tf.reduce_sum(gamma, axis=0)
+
+            # mu: [num_mixture, num_dim] mu_ij = sum(gamma_ki, z_kj)
+            mu = tf.einsum('ki, kj->ij', gamma, z) / phi_sum
+
+            # simga: [num_mixture, num_dim]
+            if self.use_diag_cov == 1:
+                # z - mu_1:K
+                z_t = (z[:, None, :] - mu[None, :, :]) ** 2
+
+                # only preserve the diag values of the sigma, each element equals to the raw sum of zt*gamma
+                sigma = tf.reduce_sum(z_t * gamma[:,:,None], 0) / phi_sum
+                dev = sigma ** 0.5
+
+                # diag values of (z-u_k)*sigma^{-1}
+                z_norm = tf.reduce_sum(z_t / sigma, 2)
+
+                t1 = tf.exp(-0.5 * z_norm)
+                t2 = ((2 * math.pi) ** (0.5 * self.z.shape[1])) * tf.reduce_prod(dev, 1)
+
+                # Likelihood
+                tmp = phi * t1 / t2
+                likelihood = tf.reduce_sum(tmp, 1)
+
+                energy = tf.reduce_mean(-tf.log(likelihood))
+                gmm_cov_diag_loss = tf.reduce_sum(1.0 / (sigma+1e-8))
+
+            else:
+                z_centered = tf.sqrt(gamma[:, :, None]) * (z[:, None, :] - mu[None, :, :])
+                sigma = tf.einsum('ikl,ikm->klm', z_centered, z_centered) / phi_sum[:, None, None]
+                n_features = z.shape[1]
+                min_vals = tf.diag(tf.ones(n_features, dtype=tf.float32)) * 1e-6
+                L = tf.cholesky(sigma + min_vals[None, :, :])
+                z_centered = z[:, None, :] - mu[None, :, :]  # ikl
+                v = tf.matrix_triangular_solve(L, tf.transpose(z_centered, [1, 2, 0]))  # kli
+
+                # log(det(Sigma)) = 2 * sum[log(diag(L))]
+                log_det_sigma = 2.0 * tf.reduce_sum(tf.log(tf.matrix_diag_part(L)), axis=1)
+
+                # To calculate energies, use "log-sum-exp" (different from orginal paper)
+                d = z.get_shape().as_list()[1]
+                logits = tf.log(phi[:, None]) - 0.5 * (tf.reduce_sum(tf.square(v), axis=1)+ d * tf.log(2.0 * np.pi) + log_det_sigma[:, None])
+                energy = - tf.reduce_logsumexp(logits, axis=0)
+                gmm_cov_diag_loss = tf.reduce_sum(tf.divide(1, tf.matrix_diag_part(sigma)))
+
         return energy, gmm_cov_diag_loss
 
     def build_model(self):
@@ -106,7 +155,7 @@ class GAGMM(object):
             self.build_model()
             with tf.Session() as sess:
                 tf.global_variables_initializer().run()
-                self.saver = tf.train.saver()
+                self.saver = tf.train.Saver()
                 num_batch = X.shape[0] // batch
                 idx = np.arange(X.shape[0])
                 # start training...
