f232852758477ec3e2e452f3e5d8f88e970b55ed
Henrygwb <henrygwb@gmail.com>
2019-01-18 14:50:28 -0500

    add dagmm done

--- src/dagmm.py
+++ src/dagmm.py
@@ -7,7 +7,8 @@ metrics = metrics()
 from os import makedirs
 from os.path import exists, join
 import math
-class GAGMM(object):
+
+class DAGMM(object):
     def __init__(self,
                  compress_hidden,
                  estimate_hidden,
@@ -19,7 +20,7 @@ class GAGMM(object):
                  estimate_activation = tf.nn.tanh,
                  dropout_rate = 0.5,
                  use_dropout = 1,
-                 use_diag_cov = 0
+                 use_diag_cov = 1
                  ):
         self.input_dim = compress_hidden[0]
         self.compress_hidden = compress_hidden
@@ -53,11 +54,11 @@ class GAGMM(object):
         norm_x_prime = tf.norm(x_prime, ord=2, axis=1)
         norm_diff = tf.norm((x - x_prime), ord=2, axis=1)
         euclidean = norm_diff / (norm_x + 1e-4)
-        cosine = tf.reduce_sum(tf.multiply(x, x_prime), axis=1)/(tf.multiply(norm_x * norm_x_prime) + 1e-4)
-        #cosine = 0.5 * (1 - tf.reduce_sum(tf.multiply(x, x_prime), axis=1) / (tf.multiply(norm_x * norm_x_prime) + 1e-4))
+        #cosine = tf.reduce_sum(tf.multiply(x, x_prime), axis=1)/(tf.multiply(norm_x, norm_x_prime) + 1e-4)
+        cosine = 0.5 * (1 - tf.reduce_sum(tf.multiply(x, x_prime), axis=1) / (tf.multiply(norm_x, norm_x_prime) + 1e-4))
 
-        zr = tf.concat([euclidean, cosine], axis=1)
-        z = tf.concat([zc, zr])
+        zr = tf.concat([euclidean[:,None], cosine[:,None]], axis=1)
+        z = tf.concat([zc, zr], axis=1)
         return z, x_prime
 
     def build_estimate(self, z):
@@ -65,12 +66,12 @@ class GAGMM(object):
         with tf.variable_scope('estimator'):
             for i in self.estimate_hidden:
                 layer += 1
-                if i != self.estimate_hidden[:-1]:
-                    z = tf.layers.dense(z, activation=self.estimate_activation, name='estimator_%d' % (layer))
+                if i != self.estimate_hidden[-1]:
+                    z = tf.layers.dense(z, i, activation=self.estimate_activation, name='estimator_%d' % (layer))
                     if self.use_dropout == 1:
                         z = tf.layers.dropout(z, rate=self.dropout_rate, name='estimator_%d' % (layer))
                 else:
-                    logits = tf.layers.dense(z, name='estimator_logit')
+                    logits = tf.layers.dense(z, i, name='estimator_logit')
             gamma = tf.nn.softmax(logits, name='estimator_softmax')
         return gamma
 
@@ -81,7 +82,7 @@ class GAGMM(object):
             phi_sum = tf.reduce_sum(gamma, axis=0)
 
             # mu: [num_mixture, num_dim] mu_ij = sum(gamma_ki, z_kj)
-            mu = tf.einsum('ki, kj->ij', gamma, z) / phi_sum
+            mu = tf.einsum('ki,kj->ij', gamma, z) / phi_sum[:,None]
 
             # simga: [num_mixture, num_dim]
             if self.use_diag_cov == 1:
@@ -89,37 +90,39 @@ class GAGMM(object):
                 z_t = (z[:, None, :] - mu[None, :, :]) ** 2
 
                 # only preserve the diag values of the sigma, each element equals to the raw sum of zt*gamma
-                sigma = tf.reduce_sum(z_t * gamma[:,:,None], 0) / phi_sum
+                sigma = tf.reduce_sum(z_t * gamma[:,:,None], 0) / phi_sum[:,None]
                 dev = sigma ** 0.5
 
                 # diag values of (z-u_k)*sigma^{-1}
                 z_norm = tf.reduce_sum(z_t / sigma, 2)
 
                 t1 = tf.exp(-0.5 * z_norm)
-                t2 = ((2 * math.pi) ** (0.5 * self.z.shape[1])) * tf.reduce_prod(dev, 1)
+                t2 = ((2 * math.pi) ** (0.5 * z.shape[1]._value)) * tf.reduce_prod(dev, 1)
 
-                # Likelihood
                 tmp = phi * t1 / t2
-                likelihood = tf.reduce_sum(tmp, 1)
+                logits = tf.reduce_sum(tmp, 1)
 
-                energy = tf.reduce_mean(-tf.log(likelihood))
+                energy = -tf.log(logits)
                 gmm_cov_diag_loss = tf.reduce_sum(1.0 / (sigma+1e-8))
 
             else:
                 z_centered = tf.sqrt(gamma[:, :, None]) * (z[:, None, :] - mu[None, :, :])
                 sigma = tf.einsum('ikl,ikm->klm', z_centered, z_centered) / phi_sum[:, None, None]
-                n_features = z.shape[1]
+                n_features = z.shape[1]._value
                 min_vals = tf.diag(tf.ones(n_features, dtype=tf.float32)) * 1e-6
                 L = tf.cholesky(sigma + min_vals[None, :, :])
-                z_centered = z[:, None, :] - mu[None, :, :]  # ikl
-                v = tf.matrix_triangular_solve(L, tf.transpose(z_centered, [1, 2, 0]))  # kli
+                z_centered = z[:, None, :] - mu[None, :, :]
+
+                # sigma = LL^{T} Z^{T}sigma^{-1}Z = Z^{T}L^{T}^{-1}L^{-1}Z = (L^{-1}Z)^{T}L^{-1}Z
+                v = tf.matrix_triangular_solve(L, tf.transpose(z_centered, [1, 2, 0])) # L^{-1}Z
 
                 # log(det(Sigma)) = 2 * sum[log(diag(L))]
                 log_det_sigma = 2.0 * tf.reduce_sum(tf.log(tf.matrix_diag_part(L)), axis=1)
 
-                # To calculate energies, use "log-sum-exp" (different from orginal paper)
                 d = z.get_shape().as_list()[1]
-                logits = tf.log(phi[:, None]) - 0.5 * (tf.reduce_sum(tf.square(v), axis=1)+ d * tf.log(2.0 * np.pi) + log_det_sigma[:, None])
+                # tf.square(v) =  (L^{-1}Z)^{T}L^{-1}Z
+                logits = tf.log(phi[:, None]) - 0.5 * (tf.reduce_sum(tf.square(v), axis=1)+
+                                                       d * tf.log(2.0 * np.pi) + log_det_sigma[:, None])
                 energy = - tf.reduce_logsumexp(logits, axis=0)
                 gmm_cov_diag_loss = tf.reduce_sum(tf.divide(1, tf.matrix_diag_part(sigma)))
 
@@ -147,36 +150,39 @@ class GAGMM(object):
         #self.train_op = tf.contrib.training.create_train_op(loss, self.optimizer, self.var)
         return 0
 
-    def fit(self, X, y, batch, epoch, display_interval):
+    def fit(self, X, batch, epoch, display_interval):
         print 'Strat training...'
         print '================================================='
         with tf.Graph().as_default() as graph:
             self.graph = graph
             self.build_model()
-            with tf.Session() as sess:
-                tf.global_variables_initializer().run()
-                self.saver = tf.train.Saver()
-                num_batch = X.shape[0] // batch
-                idx = np.arange(X.shape[0])
-                # start training...
-                for step in range(epoch):
-                    bar = ProgressBar(num_batch, fmt=ProgressBar.FULL)
-                    for i in num_batch:
-                        feed_dict = {self.x: X[idx[i*batch:min((i+1)*batch, X.shape[0])]]}
-                        sess.run(self.train_op, feed_dict)
-                        bar.current += 1
-                        bar()
-                        sleep(0.1)
-                    if step % display_interval == 0:
-                        loss = sess.run(self.loss, {self.x:X})
-                        print("Epoch %d/%d: loss = %.5f" % (step+1, epoch, loss))
+            self.sess = tf.Session(graph=self.graph)
+            self.sess.run(tf.global_variables_initializer())
+            self.saver = tf.train.Saver()
+            num_batch = X.shape[0] // batch
+            idx = np.arange(X.shape[0])
+            # start training...
+            for step in xrange(epoch):
+                for i in xrange(num_batch):
+                    feed_dict = {self.x: X[idx[i*batch:min((i+1)*batch, X.shape[0])]]}
+                    self.sess.run(self.train_op, feed_dict)
+                if step % display_interval == 0:
+                    bar = ProgressBar(display_interval, fmt=ProgressBar.FULL)
+                    loss = self.sess.run(self.loss, {self.x:X})
+                    print("Epoch %d/%d: loss = %.5f" % (step, epoch, loss))
+                bar.current += 1
+                bar()
+                sleep(0.1)
+            # energy = self.sess.run(self.energy, feed_dict={self.x: X})
+            # print energy
+            self.saver = tf.train.Saver()
         return 0
 
-    def evaluate(self, X, y, cutoff):
-        cutoff = X.shape[0]*cutoff
+    def evaluate(self, X, y, thred):
         energy = self.sess.run(self.energy, feed_dict={self.x: X}).flatten()
-        pred = np.zeros_like(energy)
-        pred[np.argsort(energy)[::-1][0:cutoff]] = 1
+        anomaly_energy_threshold = np.percentile(energy, thred)
+        print("Energy value at thleshold %d is %.3f" % (thred, anomaly_energy_threshold))
+        pred = np.where(energy >= anomaly_energy_threshold, 1, 0)
         acc = np.round(metrics.acc(y, pred), 5)
         nmi = np.round(metrics.nmi(y, pred), 5)
         ari = np.round(metrics.ari(y, pred), 5)
@@ -184,11 +190,11 @@ class GAGMM(object):
         print('acc = %.5f, nmi = %.5f, ari = %.5f' % (acc, nmi, ari))
         return 0
 
-    def predict(self, X, cutoff):
-        cutoff = X.shape[0]*cutoff
+    def predict(self, X, thred):
         energy = self.sess.run(self.energy, feed_dict={self.x: X}).flatten()
-        pred = np.zeros_like(energy)
-        pred[np.argsort(energy)[::-1][0:cutoff]] = 1
+        anomaly_energy_threshold = np.percentile(energy, thred)
+        print("Energy value at thleshold %d is %.3f" % (thred, anomaly_energy_threshold))
+        pred = np.where(energy >= anomaly_energy_threshold, 1, 0)
         return energy, pred
 
     def save(self, dir, model_name):
@@ -200,6 +206,9 @@ class GAGMM(object):
     def restore(self, model_path):
         with tf.Graph().as_default() as graph:
             self.graph = graph
+            self.build_model()
             self.sess = tf.Session(graph=graph)
-            self.saver = tf.train.import_meta_graph(model_path+'.meta')
+            self.sess.run(tf.global_variables_initializer())
+            self.saver = tf.train.Saver(self.var)
             self.saver.restore(self.sess, model_path)
+        return 0
